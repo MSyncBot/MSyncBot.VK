c88d57dbb5f6ad7cf42ec975253b2914806c4e2a
Neelov Eugene <neelov-eo@mambastick.ru>
2024-02-14 19:17:20 +0300

    Create project

---
+++ MSyncBot.VK/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Enums.StringEnums;
+using VkNet.Model;
+
+namespace MSyncBot.VK;
+
+abstract class Program
+{
+    static async Task Main(string[] args)
+    {
+        var api = new VkApi();
+        
+        api.Authorize(new ApiAuthParams
+        {
+            AccessToken = "",
+            Settings = Settings.All,
+        });
+        
+        try
+        {
+            while (true)
+            {
+                var longPollResponse = await api.Groups.GetLongPollServerAsync(224017484);
+                var updates = await api.Groups.GetBotsLongPollHistoryAsync(new BotsLongPollHistoryParams
+                {
+                    Ts = longPollResponse.Ts,
+                    Key = longPollResponse.Key,
+                    Server = longPollResponse.Server,
+                    Wait = 25
+                });
+
+                if (updates.Updates is null) continue;
+                if (updates.Updates.Count == 0) continue;
+                
+                var handleUpdatesTask = new Task(async () =>
+                {
+                    foreach (var update in updates.Updates)
+                    {
+                        if (update.Instance is MessageNew message)
+                        {
+                            await api.Messages.SendAsync(new MessagesSendParams()
+                            {
+                                Message = message.Message.Text,
+                                PeerId = message.Message.PeerId,
+                                RandomId = new Random().Next(0, 999999)
+                            });
+                        }
+                    }
+                    
+                });
+                handleUpdatesTask.Start();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
