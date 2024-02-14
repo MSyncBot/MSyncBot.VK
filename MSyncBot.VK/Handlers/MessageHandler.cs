31febddb98a729ae067933aa8722c12c7c871442
mambastick <neelov.ev@mail.ru>
2024-02-15 00:29:30 +0300

    Added echo

--- MSyncBot.VK/Handlers/MessageHandler.cs
+++ MSyncBot.VK/Handlers/MessageHandler.cs
@@ -7,6 +7,11 @@ public class MessageHandler
 {
     public async Task HandleMessagesAsync(VkApi bot, MessageNew message)
     {
-        
+        await bot.Messages.SendAsync(new MessagesSendParams()
+        {
+            Message = message.Message.Text,
+            PeerId = message.Message.PeerId,
+            RandomId = new Random().Next(0, 999999),
+        });
     }
 }
