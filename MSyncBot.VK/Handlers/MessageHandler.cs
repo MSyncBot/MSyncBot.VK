cc6ec993d15c46b69a24327768fe82700504f674
mambastick <neelov.ev@mail.ru>
2024-02-16 13:24:06 +0300

    Deleted echo method

--- MSyncBot.VK/Handlers/MessageHandler.cs
+++ MSyncBot.VK/Handlers/MessageHandler.cs
@@ -8,13 +8,6 @@ public class MessageHandler
 {
     public async Task HandleMessagesAsync(VkApi bot, MessageNew message, MLogger logger)
     {
-        logger.LogInformation($"{message.Message.PeerId} написал(а): {message.Message.Text}");
         
-        await bot.Messages.SendAsync(new MessagesSendParams()
-        {
-            Message = message.Message.Text,
-            PeerId = message.Message.PeerId,
-            RandomId = new Random().Next(0, 999999),
-        });
     }
 }
