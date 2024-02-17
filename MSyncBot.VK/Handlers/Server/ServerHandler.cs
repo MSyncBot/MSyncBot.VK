a027ae03ccab2d125b531456517abca14848d056
mambastick <neelov.ev@mail.ru>
2024-02-17 17:29:09 +0300

    Deleted localhost

--- MSyncBot.VK/Handlers/Server/ServerHandler.cs
+++ MSyncBot.VK/Handlers/Server/ServerHandler.cs
@@ -18,8 +18,8 @@ public class ServerHandler(string? address, int port, VkApi bot) : WsClient(addr
     public override void OnWsConnecting(HttpRequest request)
     {
         request.SetBegin("GET", "/");
-        request.SetHeader("Host", "localhost");
-        request.SetHeader("Origin", "http://localhost");
+        request.SetHeader("Host", address);
+        request.SetHeader("Origin", $"http://{address}");
         request.SetHeader("Upgrade", "websocket");
         request.SetHeader("Connection", "Upgrade");
         request.SetHeader("Sec-WebSocket-Key", Convert.ToBase64String(WsNonce));
